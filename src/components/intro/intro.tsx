c2a440a1aeb0fe736861fd65bff50044caf66f02
J Builds <jbuildsdev@outlook.com>
2023-10-24 21:11:40 -0500

    Create intro component

---
+++ src/components/intro/intro.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames';
+import styles from './intro.module.scss';
+import { Animation } from '../animation/animation';
+
+export interface IntroProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const Intro = ({ className }: IntroProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <h2 about={''} className={styles.title}>J Builds</h2>
+            <div>
+                <h3 className={styles.subtitle}>Software Engineer and Designer</h3>
+                <svg
+                    height="4"
+                    viewBox="0 0 204 4"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={styles.line}
+                >
+                    <path
+                        d="M2 2L202 1.99998"
+                        stroke="url(#paint0_linear_6_519)"
+                        stroke-width="3"
+                        stroke-linecap="round"
+                    />
+                    <defs>
+                        <linearGradient
+                            id="paint0_linear_6_519"
+                            x1="202.5"
+                            y1="2"
+                            x2="0.999995"
+                            y2="2"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stop-color="#DC3284" />
+                            <stop offset="1" stop-color="#DC3232" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+                <p className={styles.text}>Web3 Coder services for crypto and front end .</p>
+                <button>Let&apos;s Build 🔨</button></div>
+            <Animation className={styles.animation} /></div>
+    );
+};
